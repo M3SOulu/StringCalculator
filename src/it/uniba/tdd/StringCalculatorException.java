7a3149bf520186b3f6ac665f33629cc4ebbcd2e4
Michele <Michele@Michele_PC.homenet.telecomitalia.it>
2016-11-30 23:00:17 +0100

    add method implemented added all tests

--- src/it/uniba/tdd/StringCalculatorException.java
+++ src/it/uniba/tdd/StringCalculatorException.java
@@ -5,4 +5,9 @@ public class StringCalculatorException extends Exception {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	public StringCalculatorException(){
+		
+		super("negative number detected!!");
+	}
 }
