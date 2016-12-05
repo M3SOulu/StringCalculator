5e5dcacade0a0404f3bf3ca6069a4392f3f83dae
simone.denisi <denisisimone@gmail.com>
2016-12-05 09:50:58 +0100

    String calculator completed

--- src/it/uniba/tdd/StringCalculator.java
+++ src/it/uniba/tdd/StringCalculator.java
@@ -1,69 +1,36 @@
 package it.uniba.tdd;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 
-	public int add(String numbersStr) {
-		String[] str=null;
-		
-		if (numbersStr.length() == 0) {
-			return 0;
-		}
-
-		if (numbersStr.length() == 1) {
-			return Integer.parseInt(numbersStr);
-		}
-
-		if (numbersStr.length() == 3) {
-			return Integer.parseInt(numbersStr.substring(0, 1)) + Integer.parseInt(numbersStr.substring(2, 3));
-		} else {
-			int tot = 0;
-			str=numbersStr.split(",");
-			for(String st:str)
-				tot=tot+Integer.parseInt(st);
-			return tot;
-		}
-
-	}
-
-	public int addSpecialChar(String numbersStr) {
-		int tot = 0;
-
-		for (int i = 0; i < numbersStr.length(); i++) {
-			if (isNumber(numbersStr.charAt(i))) {
-				tot = tot + Integer.parseInt(numbersStr.substring(i, i + 1));
-			}
-		}
-
-		return tot;
-	}
-
-	private boolean isNumber(char ciar) {
-		if (ciar == '1' || ciar == '2' || ciar == '3' || ciar == '4' || ciar == '5' || ciar == '6' || ciar == '7'
-				|| ciar == '8' || ciar == '9' || ciar == '0')
-			return true;
-
-		return false;
-	}
-
-	public int addDiffDelimiters(String numbersStr){
+	public int add(String numbersStr) throws NumberFormatException, StringCalculatorException {
 		int tot=0;
-		char delimiter=numbersStr.charAt(2);
-		String[] numbs=null;
 		
-		numbs=numbersStr.split(String.valueOf(delimiter));
+		Pattern pattern=Pattern.compile("\\d+");
+		Matcher matc=pattern.matcher(numbersStr);
 		
-		for(String str:numbs){
-			for(int i=0;i<str.length();i++){
-				if(isNumber(str.charAt(i)))
-					tot=tot+Integer.parseInt(str.substring(i, i + 1));
-			}
-			
-			
-			
-			//tot=tot+Integer.parseInt(str);
+		while(matc.find()){
+			if(isValid(numbersStr)&&Integer.parseInt(matc.group())<=1000)
+				tot+=Integer.parseInt(matc.group());
 		}
 		
 		return tot;
-		
+
 	}
+
+	public boolean isValid(String str) throws StringCalculatorException {
+		
+		Pattern pattern=Pattern.compile("-\\d+");
+		Matcher matc=pattern.matcher(str);
+		
+		while(matc.find()){
+			throw new StringCalculatorException();
+		}
+		
+		
+		return (!(str.length()==0)&&!str.endsWith("\\n"));
+	}
+
 }
--- src/it/uniba/tdd/tests/StringCalculatorTest.java
+++ src/it/uniba/tdd/tests/StringCalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import it.uniba.tdd.StringCalculator;
+import it.uniba.tdd.StringCalculatorException;
 
 
 public class StringCalculatorTest {
@@ -16,22 +17,22 @@ public class StringCalculatorTest {
 	}
 	
 	@Test
-	public void emptyString() {
+	public void emptyString() throws NumberFormatException, StringCalculatorException {
 		Assert.assertEquals(0, str.add(""));
 	}
 	
 	@Test
-	public void oneDigitString() {
+	public void oneDigitString() throws NumberFormatException, StringCalculatorException {
 		Assert.assertEquals(5, str.add("5"));
 	}
 	
 	@Test
-	public void twoDigitString() {
+	public void twoDigitString() throws NumberFormatException, StringCalculatorException {
 		Assert.assertEquals(8, str.add("5,3"));
 	}
 	
 	@Test
-	public void moreDigitsString() {
+	public void moreDigitsString() throws NumberFormatException, StringCalculatorException {
 		Assert.assertEquals(10, str.add("5,3,2"));
 		Assert.assertEquals(13, str.add("5,3,3,2"));
 		Assert.assertEquals(15, str.add("5,3,3,2,2"));
@@ -40,18 +41,18 @@ public class StringCalculatorTest {
 	}
 	
 	@Test
-	public void spCharString() {
-		Assert.assertEquals(10, str.addSpecialChar("5!,%3,2/"));
-		Assert.assertEquals(13, str.addSpecialChar("5%,3/&,3/,2"));
-		Assert.assertEquals(15, str.addSpecialChar("5:,3,;,3,2)),2"));
-		Assert.assertEquals(21, str.addSpecialChar("5£)=,3,2$,$5,%3%3"));
-		Assert.assertEquals(6, str.addSpecialChar("â€œ1\n2,3â€"));
+	public void spCharString() throws NumberFormatException, StringCalculatorException {
+		Assert.assertEquals(10, str.add("5!,%3,2/"));
+		Assert.assertEquals(6, str.add("//[\\*][%]\\n1\\*2%3"));
+		Assert.assertEquals(2, str.add("1001,2"));
+		Assert.assertFalse(str.isValid("65,gyy-grug9rfy4..ff\\n"));
+		Assert.assertEquals(6, str.add("â€œ1\n2,3â€"));
 		
 	}
 	
-	@Test
-	public void delimiterCharString() {
-		Assert.assertEquals(3, str.addDiffDelimiters("//;\n1;2"));
+	@Test (expected=StringCalculatorException.class)
+	public void delimiterCharString() throws NumberFormatException, StringCalculatorException {
+		str.add("//;\n-1;2");
 		
 	}
 
